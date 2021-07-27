38ac1619e6bba803e2c97b4cab363b1db7453b93
shazz-shahid <87892549+shazz-shahid@users.noreply.github.com>
2021-07-27 10:34:23 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/oBLYqk7.jpeg";
+  r_text[0] = "https://i.imgur.com/VJuPuTP.jpg";
     
     
     var i = Math.floor(1*Math.random())
