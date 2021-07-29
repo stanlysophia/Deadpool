955382b1f63ff1fd327c2cc941e32218160b816d
shazz-shahid <87892549+shazz-shahid@users.noreply.github.com>
2021-07-29 16:45:00 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -21,7 +21,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*😈𝕊ℍ𝔸ℍ𝕀𝔻-𝕊ℍ𝔸ℤℤ😈*
 *𝕆𝕨𝕟𝕖𝕣 𝕟𝕦𝕞𝕓𝕖𝕣 wa.me/918078391406?text=Hi*
 *𝕊𝕙𝕒𝕙𝕚𝕕-𝕤𝕙𝕒𝕫𝕫 𝔻𝕖𝕒𝕕𝕡𝕠𝕠𝕝 𝕘𝕚𝕥𝕙𝕦𝕓𝕝𝕚𝕟𝕜   https://github.com/shazz-shahid/Deadpool*
-*𝔸𝕞𝕖𝕖𝕣-𝕜𝕒𝕝𝕝𝕦𝕞𝕥𝕙𝕠𝕕𝕚 𝕡𝕚𝕜𝕒𝕔𝕙𝕦 githublink https://github.com/ameer-kallumthodi/Pikachu*
+*𝔸𝕞𝕖𝕖𝕣-𝕜𝕒𝕝𝕝𝕦𝕞𝕥𝕙𝕠𝕕𝕚 𝕡𝕚𝕜𝕒𝕔𝕙𝕦 𝕘𝕚𝕥𝕙𝕦𝕓𝕝𝕚𝕟𝕜 https://github.com/ameer-kallumthodi/Pikachu*
 `}) 
 
 }));
